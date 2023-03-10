4b9f844f2deb5b2b40e5dde45864c7fe9b7a7846
DavidGoller <gollerdavid21@gm,ail.com>
2023-03-10 22:29:35 +0100

    css 4 chat

--- src/view/chatAktion.tpl.php
+++ src/view/chatAktion.tpl.php
@@ -6,10 +6,21 @@
     <link rel="stylesheet" href="css/style.chat.css">
 </head>
 <body>
+    <div id="messages">
+        <p class="messege">
+        <label>This is a Massage</label>
+        </p>
+        <p class="messege">
+            <label>This is a Looooonger Massage</label>
+        </p>
+        <p class="ownmessege">
+            <label>This is my Massage, which I have written as of now </label>
+        </p>
+    </div>
     <div id="eingabebereich">
         <label id="benutzername"> BENUTZERNAME </label>
         <input type="text" name="eingabe" id="eingabe">
-        <button id="send">SEND</button>
+        <input type="button" id="send" value="SEND">
     </div>
 
 
