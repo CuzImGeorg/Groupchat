15838f0e6a84c8f2b4b4e69749132ce144c81cbb
DavidGoller <gollerdavid21@gm,ail.com>
2023-03-12 12:58:25 +0100

    StyleGuide.1

--- src/view/chatAktion.tpl.php
+++ src/view/chatAktion.tpl.php
@@ -3,24 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <title>Chat</title>
+    <script src="src/view/js/chat.js"></script>
     <link rel="stylesheet" href="src/view/css/style.chat.css">
 </head>
 <body>
     <div id="messages">
-        <p class="messege">
-        <label>This is a Massage</label>
-        </p>
-        <p class="messege">
-            <label>This is a Looooonger Massage</label>
-        </p>
-        <p class="ownmessege">
-            <label>This is my Massage, which I have written as of now </label>
-        </p>
+
     </div>
     <div id="eingabebereich">
         <label id="benutzername"> <?=$benutzer->getNickname()?> </label>
         <textarea name="eingabe" id="eingabe" > </textarea>
-        <input type="button" id="send" value="SEND">
+        <input type="button"  id="send" value="SEND">
     </div>
 
 
---
+++ src/view/js/chat.js
@@ -0,0 +1,56 @@
+home();
+function delay(time){
+    return new Promise(resolve => setTimeout(resolve, time));
+}
+async function home(){
+    await delay(1000);
+    setmessege("This is a Massage",true);
+    setmessege("This is my Massage, which I have written as of now",false);
+    setmessege("This is a Looooonger Massage",true);
+    console.log('Now');
+    await delay(1000);
+    setmessege("This is a later messege",true);
+    console.log('later');
+}
+
+function setmessege(string, notown){
+    const array = []
+    const strglenght =string.length;
+    let j;
+    for (let i = 80; i <= strglenght; i+=80){
+        const thisstr =string.slice(i-80,i)
+        array.push(thisstr);
+        j=i;
+    }
+    const idiff= strglenght-j;
+    let laststr =string.slice(strglenght-idiff,strglenght);
+    if (strglenght>80){
+        let l = laststr.length;
+        while(l<80){
+            l++;
+            laststr=laststr+" ";
+        }
+    }
+    array.push(laststr);
+
+    let m = notown ? "messege" : "ownmessege";
+    let ret = "";
+    array.forEach(sliceString)
+
+    function sliceString(value, pos, array){
+    value=value.replaceAll(' ',"&nbsp;");
+    if (pos==0 && pos==array.length-1) ret+="<label class='oneliner'>"+value+"</label>";
+    else if (pos==0) ret+="<label class='firsttext'>"+value+"</label>";
+    else if (pos==array.length-1)ret+="<label class='lasttext'>"+value+"</label>";
+    else ret+="<label class='midtext'>"+value+"</label>";
+    ret+="<br>";
+    }
+
+    let p=document.createElement('p');
+    console.log(p);
+    p.classList.add(m);
+    p.innerHTML=ret;
+    document.getElementById('messages').appendChild(p);
+
+}
+
