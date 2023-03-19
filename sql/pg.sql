11cbeaf56639529d5e6547a12bc6918372ddaa75
georg <georg>
2023-03-19 15:02:59 +0100

    xml fix

--- sql/pg.sql
+++ sql/pg.sql
@@ -8,13 +8,23 @@ CREATE TABLE benutzer(
 
 );
 
+INSERT INTO benutzer(vorname, nachname, nickname, password) VALUES ('admin', 'admin', 'admin' ,'sadasdakksdasdasdsadasd');
+
 CREATE TABLE message(
 	id serial PRIMARY KEY,
 	msgtext varchar(1023),
 	time TIMESTAMP,
-       	benutzerid int,
+    benutzerid int,
 
 	FOREIGN KEY(benutzerid) REFERENCES benutzer(id) 
- 
 
 );
+
+INSERT INTO message (msgtext, time, benutzerid) VALUES ('Willkommen und Abschied beim Chat! Ich hoffe Sie haben viele Glücksgefühle mit ' ||
+                                                        'Ihren sogennanten Freunden sich eine Liste von Buchstaben zu schicken ' ||
+                                                        'die von einer Datenverarbeitungsanlage in ein elektronischen Karteikasten ' ||
+                                                        'für bessere Zeiten aufbewährt werden um sie von anderen Teilnehmern der Zeichenfolge' ||
+                                                        'austauschanwendung entgegenzunehmen und zu interpretiern in der Zuversicht ebenfahs ein größe ' ||
+                                                        'Ahnzahl  von 0 en und 1 sen zu erhalten die ein Magisches Gerät in eine Für Mitglieder der Spezies Mensch ' ||
+                                                        'verstänlichen Ausdruckweiße trasformiert. '
+, now(), 1);
